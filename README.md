b8a08bfbf182a2d2a74245ef67847d108abf53fa
Kashif Shabir <kashif.shabir12@gmail.com>
2024-03-25 14:30:45 +0100

    Requirements added

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # End-2-End-OD
 It will be an end to end OD for sign language
+
+
+## How to run
+
+```bash
+conda create -n signlang python=3.7 -y
+```
+
+```bash
+conda activate signlang
+```
+
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'Kashif Shabir',
+    author_email= 'kashif.shabir12@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
